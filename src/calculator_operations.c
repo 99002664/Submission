fd1bc5115ba11c3240399b5997ee8f1d7c6be8c7
99002664 <71490590+99002664@users.noreply.github.com>
2020-09-18 22:47:52 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -31,3 +31,22 @@ int greater(int operand1, int operand2)
         return operand2;
 }
 
+int smaller(int operand1, int operand2)
+{
+    if(operand1 < operand2)
+        return operand1;
+    else
+        return operand2;
+}
+
+int rectarea(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int rectperi(int operand1, int operand2)
+{
+    return 2*(operand1 + operand2)
+}
+    
+
