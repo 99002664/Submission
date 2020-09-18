f69de9a113c4f5b6a9aa3ffb9eb9e9a51cb74cb1
99002664 <71490590+99002664@users.noreply.github.com>
2020-09-18 23:10:51 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -48,5 +48,21 @@ int rectperi(int operand1, int operand2)
 {
     return 2*(operand1 + operand2);
 }
-    
+
+int prime(int operand1)
+{
+    int var,number=0,flag=0;     
+    number=operand1/2;    
+    for(var=2;var<=number;var++)    
+    {    
+    if(operand1%var==0)    
+    {    
+    return 0;   
+    flag=1;    
+    break;    
+    }    
+    }    
+    if(flag==0)         
+    return 1;  
+}
 
