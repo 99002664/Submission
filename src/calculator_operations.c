44e64f5c1d04c6f1c82933c5eb5d4b3badacf8dc
99002664 <71490590+99002664@users.noreply.github.com>
2020-09-19 08:51:13 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -167,6 +167,97 @@ int factorial(int operand1)
         return fact;
         }
     }
+int adder3bit(int operand1, int operand2, int operand3)
+
+{
+    if(operand1 < 0 || operand2 < 0 || operand3 < 0)
+          {
+             return -1;
+          }
+     else if (operand1 > 1 || operand2 > 1 || operand3 > 1)
+          {
+              return -1;
+           }
+     else
+        {
+           if(operand1==0 && operand2==0 && operand3==0)
+              {
+               return 0;
+              }
+           else if(operand1==0 && operand2==0 && operand3==1)
+              {
+               return 1;
+              }
+            else if(operand1==0 && operand2==1 && operand3==0)
+               {
+               return 1;
+               }
+        else if(operand1==0 && operand2==1 && operand3==1)
+              {
+               return 0;
+               }
+       else if(operand1==1 && operand2==0 && operand3==0)
+               {
+               return 1;
+               }
+       else if(operand1==1 && operand2==0 && operand3==1)
+              {
+               return 0;
+               }
+       else if(operand1==1 && operand2==1 && operand3==0)
+               {
+               return 0;
+               }
+        else 
+              return 1;
+     }
+ }
+int subtractor3bit(int operand1, int operand2, int operand3)
+{
+    if(operand1 < 0 || operand2 < 0 || operand3 < 0)
+    {
+        return -1;
+    }
+    else if (operand1 > 1 || operand2 > 1 || operand3 > 1)
+    {
+        return -1;
+    }
+    else
+    {
+        if(operand1==0 && operand2==0 && operand3==0)
+        {
+            return 0;
+        }
+        else if(operand1==0 && operand2==0 && operand3==1)
+        {
+            return 1;
+        }
+        else if(operand1==0 && operand2==1 && operand3==0)
+        {
+            return 1;
+        }
+        else if(operand1==0 && operand2==1 && operand3==1)
+        {
+            return 0;
+        }
+        else if(operand1==1 && operand2==0 && operand3==0)
+        {
+            return 1;
+        }
+        else if(operand1==1 && operand2==0 && operand3==1)
+        {
+            return 0;
+        }
+        else if(operand1==1 && operand2==1 && operand3==0)
+        {
+            return 0;
+        }
+        else if(operand1==1 && operand2==1 && operand3==1)
+        {
+            return 1;
+        }
+    }
+}
 
 
 
