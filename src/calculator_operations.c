d62445a704e3c1b3e15ddf90845f011474c07aa1
99002664 <71490590+99002664@users.noreply.github.com>
2020-09-19 08:30:02 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -81,3 +81,92 @@ int bitwisenot(int operand1)
     return !operand1;
 }
 
+int modulus(int operand1, int operand2)
+{
+    return operand1 % operand2;
+}
+
+int square(int operand1)
+{
+    int result;
+    result= operand1 * operand1;
+    return result;
+}
+int and(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 1;
+    else if(operand1==1 && operand2==1)
+    return 1;
+    else
+    return 0;
+}
+int or(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 1;
+    else if(operand1==0 && operand2==0)
+    return 0;
+    else
+    return 1;
+}
+
+int not(int operand1)
+{
+    if(operand1==0)
+        return 1;
+    else
+        return 0;
+}
+int nand(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==1 && operand2==1)
+    return 0;
+    else
+    return 1;
+}
+int nor(int operand1, int operand2)
+{
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==0 && operand2==0)
+    return 1;
+    else
+    return 0;
+}
+int factorial(int operand1)
+    {   
+        int fact=1;
+        if (operand1==0)
+        {
+            return 1;
+        }
+        if (operand1 < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = 1; i <= operand1; ++i)
+            {
+                fact *= i;
+            }
+        return fact;
+        }
+    }
+
+
+
